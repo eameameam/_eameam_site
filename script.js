ca86f9668fa21da667bb74b8c7c45074dff82389
eameameam <eam.motion@gmail.com>
2023-10-30 22:44:33 +0100

    Start

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-function showPage(pageId) {
-    const pages = document.querySelectorAll('.page');
-    pages.forEach(page => {
-      if (page.id === pageId) {
-        page.style.transform = "translateX(0%)";
-      } else {
-        page.style.transform = "translateX(100%)";
-      }
-    });
-  } 
+document.addEventListener("DOMContentLoaded", function() {
+  const contactBtn = document.getElementById("contactBtn");
+  const popup = document.getElementById("popup");
+  const closeBtn = document.getElementById("closeBtn");
+  const content = document.getElementById("content");
+  const overlay = document.getElementById("overlay");
+
+  contactBtn.addEventListener("click", function() {
+    popup.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+    content.classList.add("blur");
+  });
+
+  closeBtn.addEventListener("click", function() {
+    popup.classList.add("hidden");
+    overlay.classList.add("hidden");
+    content.classList.remove("blur");
+  });
+
+  overlay.addEventListener("click", function() {
+    popup.classList.add("hidden");
+    overlay.classList.add("hidden");
+    content.classList.remove("blur");
+  });
+});
